2bbc63a87a3bbf8ddf7eaf1eee72195c443b93c6
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-04-25 16:40:43 +0200

    path change

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,13 @@
-  
+const mobNav = document.getElementsByClassName("mob_nav");
+
+function navFunction() {
+        
+  if( mobNav.style.display === 'block'){
+      mobNav.style.display = 'none';
+  } else {
+      mobNav.style.display = "block";
+  }
+
+}
+
+navFunction();
